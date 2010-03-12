51b340b864cf9c090bbbbddfb2cb6733ef05f59d
unknown <matt.snyder@.(none)>
2010-03-12 09:33:50 -0500

    added comments to test and rhino mock

--- StackTests/StackTests.cs
+++ StackTests/StackTests.cs
@@ -1,5 +1,14 @@
 using NUnit.Framework;
 
+/*
+ * Supports push
+ * Pop returns last object pushed
+ * Can ask if empty
+ * Limit to ten items (throw ex if push when full)
+ * throw ex if pop and is empty
+ * 
+ */
+
 namespace StackTests
 {
     public class StackTests
